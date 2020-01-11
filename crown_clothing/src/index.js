7d3ee4d298f51a5475a00cbc0a5ac840737d1e06
Philip Ralston <philr1699@gmail.com>
2020-01-11 21:20:34 +0000

    11/01/2020

--- crown_clothing/src/index.js
+++ crown_clothing/src/index.js
@@ -10,7 +10,7 @@ import App from './App';
 ReactDOM.render(
  // BrowserRouter is a component that we wrap around our application
  // Gives our application all the functionality of routing that the React Router DOM library provides
- // Provider - component from React Redux - wrap around entire application because we want everything inside to have access to the store object that we get from Redux
+ // Provider - component from React Redux - wrap around entire application because we want everything inside to have access to the store (App State) that we get from Redux
  <Provider>
   <BrowserRouter>
     <App/>
